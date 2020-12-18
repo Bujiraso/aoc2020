7dbbeb18ea3ca9764bc4189fc1e0e0a28c7f041b
Bujiraso <bujiraso@hotmail.ca>
2020-12-18 10:07:17 -0400

    Make performance even worse
    Dudn't matter.
    Program completed

--- day1.sh
+++ day1.sh
@@ -1,12 +1,20 @@
 #!/usr/bin/env bash
 
 inputFi=${1}
-input=$(cat "${inputFi}")
+input=$(cat "${inputFi}" | sort )
 function compute() {
-    echo "${input}" \ | while read outer; do 
-        echo "${input}" \ | while read inner; do
-            if [[ $((outer + inner)) == 2020 ]]; then
-                echo $((outer * inner))
+    echo "${input}" \ | while read outerer; do
+        if [[ ${outerer} -gt 2000 ]]; then
+            continue
+        fi
+        echo "${input}" \ | while read outer; do
+            echo "${input}" \ | while read inner; do
+                if [[ $((outer + inner + outerer)) == 2020 ]]; then
+                    echo $((outer * inner * outerer))
+                    exit 5
+                fi
+            done
+            if [[ $? -eq 5 ]]; then
                 exit 5
             fi
         done
